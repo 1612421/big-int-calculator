483f16d85cc8fb71dbd03527297689c74453eab2
1612421 <nguyenngocnghia53@gmail.com>
2021-12-02 01:22:38 +0700

    remove route in api.php

--- routes/api.php
+++ routes/api.php
@@ -19,6 +19,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::prefix('/')->group(function () {
-    Route::post('/calculations', [CalculatorController::class, 'calculate']);
-});
+//Route::prefix('/')->group(function () {
+//    Route::post('/calculations', [CalculatorController::class, 'calculate']);
+//});
